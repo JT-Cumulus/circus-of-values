fcb0ada7a107a138780160ddfd9763ceaf1b8302
Jiatong <jiatong1998@yahoo.com>
2022-12-14 13:08:33 +0100

    create foundations for rps in web

---
+++ python/matrix.py
@@ -0,0 +1,8 @@
+def minFallingPathSum(self, matrix) -> int:
+        for rows in matrix:
+            print(rows)
+
+matrix = [[2,1,3],[6,5,4],[7,8,9]]
+
+print(matrix[0][0])
+minFallingPathSum(matrix)

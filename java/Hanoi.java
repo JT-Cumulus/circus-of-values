ef2c84d2414847ebf4fac3eb5ff10015e3d56f71
Jiatong <jiatong1998@yahoo.com>
2023-06-13 10:44:58 +0200

    implement recursive hanoi solution

---
+++ java/Hanoi.java
@@ -0,0 +1,14 @@
+import javax.print.attribute.standard.Destination;
+
+public class Hanoi {
+    public void solve(int n, char source, char middle, char dest){
+        if (n==0) {
+            System.out.println("plate " + n + " from " + source + " to " + dest);
+            return;
+        }
+
+        solve(n-1, source, dest, middle);
+        System.out.println("plate " + n + " from " + source + " to " + dest);
+        
+    }
+}
